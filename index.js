b50aba78f86af0600580d5e1257d11e0c8ae4ea8
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-03-04 21:53:53 +0200

    puhelinluettelobackend step8

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+
+morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''))
+app.use(morgan(':method :url :status :response-time ms :body'))
 
 let persons = [
     { 
