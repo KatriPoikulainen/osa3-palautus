e8c28edd7913ee461ea3dd6593f53652c2d103cc
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-03-06 20:23:36 +0200

    puhelinluettelojatietokantastep5step17

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -47,6 +47,21 @@ app.post('/api/persons/', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
