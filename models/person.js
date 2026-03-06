596fdd5059a4379ea715db4cfb546a23c6dc8481
Katri Poikulainen <katri.poikulainen@student.hamk.fi>
2026-03-06 21:13:00 +0200

    puhelinluettelojatietokantastep8step20

--- models/person.js
+++ models/person.js
@@ -17,7 +17,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true}
+    required: true,
+    minlength: 8,
+    validate: {
+        validator: function(v) {
+            return /^\d{2,3}-\d+$/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number`
+    }}
 })
 
 personSchema.set('toJSON',{
